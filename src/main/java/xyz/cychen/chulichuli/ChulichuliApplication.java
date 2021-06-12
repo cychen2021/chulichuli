3e438a6f62681fa2e00ca81c1b389d79f9576580
Chuyang Chen <chuyangchen@foxmail.com>
2021-06-12 14:18:23 +0800

    The first commit.

---
+++ src/main/java/xyz/cychen/chulichuli/ChulichuliApplication.java
@@ -0,0 +1,13 @@
+package xyz.cychen.chulichuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChulichuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChulichuliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/cychen/chulichuli/ChulichuliApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cychen.chulichuli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChulichuliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
